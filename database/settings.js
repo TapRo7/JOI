63229764eec8679e83a0b2ab822dbf871991a0bd
TapRo <talha.saleem7077@gmail.com>
2025-07-26 19:27:10 +0500

    generalized collection names

--- database/settings.js
+++ database/settings.js
@@ -1,45 +1,48 @@
 const { insertOne, findOne, deleteOne, updateOne, findAll } = require('./index');
 
+const GUILD_COLLECTION = 'guildConfigurations';
+const USER_COLLECTION = 'userConfigurations';
+
 // Guild Configuration Functions
 async function addGuildConfiguration(guildId, webhookUrl) {
-    return insertOne('guildConfigurations', { guildId, webhookUrl });
+    return insertOne(GUILD_COLLECTION, { guildId, webhookUrl });
 }
 
 async function deleteGuildConfiguration(guildId) {
-    return deleteOne('guildConfigurations', { guildId });
+    return deleteOne(GUILD_COLLECTION, { guildId });
 }
 
 async function editGuildConfiguration(guildId, webhookUrl) {
-    return updateOne('guildConfigurations', { guildId }, { webhookUrl });
+    return updateOne(GUILD_COLLECTION, { guildId }, { webhookUrl });
 }
 
 async function getGuildConfigurations() {
-    return findAll('guildConfigurations');
+    return findAll(GUILD_COLLECTION);
 }
 
 async function getGuildConfiguration(guildId) {
-    return await findOne('guildConfigurations', { guildId });
+    return await findOne(GUILD_COLLECTION, { guildId });
 }
 
 // User Configuration Functions
 async function addUserConfiguration(userId, timezone) {
-    return insertOne('userConfigurations', { userId, timezone });
+    return insertOne(USER_COLLECTION, { userId, timezone });
 }
 
 async function deleteUserConfiguration(userId) {
-    return deleteOne('userConfigurations', { userId });
+    return deleteOne(USER_COLLECTION, { userId });
 }
 
 async function editUserConfiguration(userId, timezone) {
-    return updateOne('userConfigurations', { userId }, { timezone });
+    return updateOne(USER_COLLECTION, { userId }, { timezone });
 }
 
 async function getUserConfigurations() {
-    return findAll('userConfigurations');
+    return findAll(USER_COLLECTION);
 }
 
 async function getUserConfiguration(userId) {
-    return await findOne('userConfigurations', { userId });
+    return await findOne(USER_COLLECTION, { userId });
 }
 
 module.exports = { addGuildConfiguration, deleteGuildConfiguration, editGuildConfiguration, getGuildConfigurations, getGuildConfiguration, addUserConfiguration, deleteUserConfiguration, editUserConfiguration, getUserConfigurations, getUserConfiguration };
